a8446e77c5d7a8efebff72c85344a974bacaf659
ArunArivanandam <arunarivanandam@gmail.com>
2024-11-18 09:27:10 +0530

    add delete and get all product end-points

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,21 @@ app.get("/", (req,res)=>{
     res.send("Server is ready")
 })
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products});
+    } catch (error){
+        console.log("error in fetching products:", error.message);
+        res.status(500).json({success: false, message: "Server Error"})
+    }
+} )
+
 app.post("/api/products", async (req,res) => {
     const product = req.body; // user will send this data
 
     if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success:false, message: "Please provide all fields"})
+        return res.status(400).json({success: false, message: "Please provide all fields"})
     }
 
     const newProduct = new Product(product)
@@ -28,6 +38,18 @@ app.post("/api/products", async (req,res) => {
     } catch (error) {
         console.error("Error in Create product:", error.message);
         res.status(500).json({success: false, message: "Server Error"})
+    }
+})
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted"})
+    } catch (error){
+        console.error("Error in deleting product:", error.message);
+        res.status(404).json({success: false, message: "Product not found"})
     }
 })
 
